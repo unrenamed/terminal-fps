50ce1b41e77f5da97f6625c4efe930111c01f1ce
unrenamed <renkonazbka@gmail.com>
2023-01-22 17:21:07 +0200

    feat: use ray-casting to shade tile corners
    This will also update some constants changed during testing.

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ use std::f32::consts::PI;
 use std::io::{self, Write};
 use std::time::{Instant, Duration};
 
-const SCREEN_WIDTH: usize = 230;
-const SCREEN_HEIGHT: usize = 50;
+const SCREEN_WIDTH: usize = 120;
+const SCREEN_HEIGHT: usize = 40;
 
 const MAP_HEIGHT: usize = 16;
 const MAP_WIDTH: usize = 16;
 
-const FOV: f32 = PI / 3.0;
-const DEPTH: f32 = 30.0;
+const FOV: f32 = PI / 4.0;
+const DEPTH: f32 = 16.0;
 const SPEED: f32 = 5.0;
 
 fn main() -> Result<()> {
@@ -24,8 +24,8 @@ fn main() -> Result<()> {
     let mut lock = stdout.lock();
     execute!(lock, EnterAlternateScreen, cursor::Hide)?;
 
-    let mut player_x: f32 = 2.0;
-    let mut player_y: f32 = 1.0;
+    let mut player_x: f32 = 2.45;
+    let mut player_y: f32 = 1.35;
     let mut player_a: f32 = 0.0;
 
     let mut screen = [' '; SCREEN_WIDTH * SCREEN_HEIGHT];
@@ -41,8 +41,8 @@ fn main() -> Result<()> {
         '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '#',
         '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '#',
         '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#',
-        '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#',
-        '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#',
+        '#', '.', '.', '#', '#', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '#',
+        '#', '.', '.', '#', '#', '.', '.', '.', '#', '#', '.', '.', '.', '.', '.', '#',
         '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#',
         '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#',
         '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#',
@@ -58,7 +58,9 @@ fn main() -> Result<()> {
 
             let mut distance_to_wall = 0.0 as f32;
             let step_size = 0.1 as f32;
+            
             let mut hit_wall = false;
+            let mut boundary = false;
 
             let eye_x = ray_angle.sin();
             let eye_y = ray_angle.cos();
@@ -72,6 +74,38 @@ fn main() -> Result<()> {
                     distance_to_wall = DEPTH;
                 } else if map[(cx * MAP_WIDTH + cy)] == '#' {
                     hit_wall = true;
+
+                    let mut boundaries: Vec<(f32, f32)> = Vec::new();
+                    for tx in 0..2 {
+                        for ty in 0..2 {
+                            let vy = (cy as f32) + (ty as f32) - player_y;
+                            let vx = (cx as f32) + (tx as f32) - player_x;
+                            let d = (vx * vx + vy * vy).sqrt();
+                            let dot = (eye_x * vx / d) + (eye_y * vy / d);
+                            boundaries.push((d, dot));
+                        }
+                    }
+
+                    // sort Pairs from closest to farthest
+                    boundaries.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+
+                    // first three are closest (we will never see all four)
+                    let bound = 0.01;
+                    if let Some(b) = boundaries.get(0) {
+                        if b.1.acos() < bound {
+                            boundary = true;
+                        }
+                    }
+                    if let Some(b) = boundaries.get(1) {
+                        if b.1.acos() < bound {
+                            boundary = true;
+                        }
+                    }
+                    if let Some(b) = boundaries.get(2) {
+                        if b.1.acos() < bound {
+                            boundary = true;
+                        }
+                    }
                 }
             }
 
@@ -81,11 +115,15 @@ fn main() -> Result<()> {
             let mut shade: char;
 
             match distance_to_wall {
-                d if d <= DEPTH / 3.0 => shade = '\u{2588}',
-                d if d < DEPTH / 2.0 => shade = '\u{2593}',
-                d if d < DEPTH / 1.5 => shade = '\u{2592}',
+                d if d <= DEPTH / 4.0 => shade = '\u{2588}',
+                d if d < DEPTH / 3.0 => shade = '\u{2593}',
+                d if d < DEPTH / 2.0 => shade = '\u{2592}',
                 d if d < DEPTH => shade = '\u{2591}',
                 _ => shade = ' ',
+            }
+
+            if boundary {
+                shade = ' ';
             }
 
             for y in 0..SCREEN_HEIGHT {
@@ -112,48 +150,50 @@ fn main() -> Result<()> {
         // build map
         for nx in 0..MAP_WIDTH {
             for ny in 0..MAP_HEIGHT {
-                screen[(ny + 1) * SCREEN_WIDTH + nx] = map[ny * MAP_WIDTH + nx];
+                screen[(ny) * SCREEN_WIDTH + nx] = map[ny * MAP_WIDTH + nx];
             }
         }
-        screen[(player_x as usize + 1) * SCREEN_WIDTH + player_y as usize] = 'P';
+        screen[player_x as usize * SCREEN_WIDTH + player_y as usize] = 'P';
+
+        // clean screen
+        write!(lock, "\r\x1b[H")?;
 
         // display stats
-        writeln!(lock, "\r\x1b[H")?;
         writeln!(lock, "\r\x1b[KX: {}, Y: {}, A: {}, FPS: {}", player_x, player_y, player_a, 60)?;
-        
-         // display frame with map
-         write!(lock, "\r\x1b[K")?;
-         for ny in 0..SCREEN_HEIGHT {
-             for nx in 0..SCREEN_WIDTH {
-                 write!(lock, "{}", screen[ny * SCREEN_WIDTH + nx])?;
-             }
-             writeln!(lock, "\r")?; // move to the beginning of the next line
-         }
+
+        // display frame with map
+        write!(lock, "\r\x1b[K")?;
+        for ny in 0..SCREEN_HEIGHT {
+            for nx in 0..SCREEN_WIDTH {
+                write!(lock, "{}", screen[ny * SCREEN_WIDTH + nx])?;
+            }
+            writeln!(lock, "\r")?; // move to the beginning of the next line
+        }
 
         lock.flush()?;
 
         if let Event::Key(key) = event::read()? {
             match key.code {
                 KeyCode::Left | KeyCode::Char('a') => {
-                    player_a -= SPEED * 0.05;
+                    player_a -= SPEED * 0.035;
                 }
                 KeyCode::Right | KeyCode::Char('d') => {
-                    player_a += SPEED * 0.05;
+                    player_a += SPEED * 0.035;
                 }
                 KeyCode::Up | KeyCode::Char('w') => {
-                    player_x += player_a.sin() * SPEED * 0.15;
-                    player_y += player_a.cos() * SPEED * 0.15;
+                    player_x += player_a.sin() * SPEED * 0.1;
+                    player_y += player_a.cos() * SPEED * 0.1;
                     if map[player_x as usize * MAP_WIDTH + player_y as usize] == '#' {
-                        player_x -= player_a.sin() * SPEED * 0.15;
-                        player_y -= player_a.cos() * SPEED * 0.15;
+                        player_x -= player_a.sin() * SPEED * 0.1;
+                        player_y -= player_a.cos() * SPEED * 0.1;
                     }
                 }
                 KeyCode::Down | KeyCode::Char('s') => {
-                    player_x -= player_a.sin() * SPEED * 0.15;
-                    player_y -= player_a.cos() * SPEED * 0.15;
+                    player_x -= player_a.sin() * SPEED * 0.1;
+                    player_y -= player_a.cos() * SPEED * 0.1;
                     if map[player_x as usize * MAP_WIDTH + player_y as usize] == '#' {
-                        player_x += player_a.sin() * SPEED * 0.15;
-                        player_y += player_a.cos() * SPEED * 0.15;
+                        player_x += player_a.sin() * SPEED * 0.1;
+                        player_y += player_a.cos() * SPEED * 0.1;
                     }
                 }
                 KeyCode::Esc => {
